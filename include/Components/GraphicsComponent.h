14f2a6b3dd5b66adc40404b648ce741ad5608dbb
Alex <maddogmcclure@gmail.com>
2015-07-07 08:26:11 -0400

    Updated Folder Structure.

--- include/Components/GraphicsComponent.h
+++ include/Components/GraphicsComponent.h
@@ -2,13 +2,11 @@
 // Created by alex on 6/8/15.
 //
 
-#ifndef PROTOTYPE_ACTION_GRAPHICSCOMPONENT_H
-#define PROTOTYPE_ACTION_GRAPHICSCOMPONENT_H
+#pragma once
 
 
-class GraphicsComponent {
+class GraphicsComponent
+{
 
 };
 
-
-#endif //PROTOTYPE_ACTION_GRAPHICSCOMPONENT_H
