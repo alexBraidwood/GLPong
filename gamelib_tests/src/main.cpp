5b5f9f810cce2f88fba67481c7d1be28117824dd
Alex Braidwood <alexb.devel@gmail.com>
2015-12-29 17:38:03 -0500

    Added googletest and added gamelib_tests to full build

--- gamelib_tests/src/main.cpp
+++ gamelib_tests/src/main.cpp
@@ -1,10 +1,7 @@
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 
 auto main (int argc, char* argv[]) -> int {
   ::testing::InitGoogleTest(&argc, argv);
-  ::testing::InitGoogleMock(&argc, argv);
-
   RUN_ALL_TESTS();
   std::getchar();
 }
